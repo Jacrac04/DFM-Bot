af7c14f004bc15fbc863c80d6b75fcfe34d7fe3d
Jacrac04 <jacrac04@gmail.com>
2021-02-25 10:00:48 +0000

    Initial UI

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import json
 import traceback
 import sys
 
+import tkinter.messagebox as tkm
+
 from requests import Session
 from answer_handler import AnswerHandler
 import urllib3
 
-
+from tkinter import *
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -16,23 +18,47 @@ class InvalidLoginDetails(Exception):
     pass
 
 
-class LoginFrame():
-    def __init__(self):
-        print('Ensure that your google acount is NOT linked')
-        email = input('Email')
-        password = input('Password')
+class LoginFrame(Frame):
+    def __init__(self, master):
+        super().__init__(master)
+
+        self.label_username = Label(self, text="Email")
+        self.label_password = Label(self, text="Password")
+
+        self.entry_username = Entry(self)
+        self.entry_password = Entry(self, show="*")
+
+        self.label_username.grid(row=0, sticky=E)
+        self.label_password.grid(row=1, sticky=E)
+        self.entry_username.grid(row=0, column=1)
+        self.entry_password.grid(row=1, column=1)
+
+        self.log_btn = Button(self, text="Login", command=self._login_btn_clicked)
+        self.log_btn.grid(columnspan=2)
+
+        self.pack()
+
+        # print('Ensure that your google acount is NOT linked')
+        # email = input('Email')
+        # password = input('Password')
+    
+    def _login_btn_clicked(self):
+        email = self.entry_username.get()
+        password = self.entry_password.get()
         if '@' not in email:
             email += '@utcportsmouth.org'
         try:
             Interface(email, password)
         except InvalidLoginDetails as e:
             print(e, file=sys.stderr)
+            tkm.showerror("Login error", "Incorrect Email or Password")
 
 
 class Interface:
     def __init__(self, email, password):
         self.session = Session()
         self.test_login(email, password)
+        root.destroy()
         self.handler = AnswerHandler(self.session)  
         
         self.main_loop()
@@ -72,5 +98,9 @@ class Interface:
 
 
 if __name__ == "__main__":
-    
-    lf = LoginFrame()
+    root = Tk()
+    root.protocol('WM_DELETE_WINDOW', sys.exit)
+    root.geometry('300x80')
+    root.title('DFM Login Screen')
+    lf = LoginFrame(root)
+    root.mainloop()
