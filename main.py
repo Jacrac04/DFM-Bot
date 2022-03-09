656129f4e50775db8dea8a31c31a0363cc9e601a
Jacrac04 <62761393+Jacrac04@users.noreply.github.com>
2022-03-09 11:16:44 +0000

    Changes to check_server

--- main.py
+++ main.py
@@ -66,7 +66,19 @@ class UserInterface(Tk):
         self.disable(self.gf.winfo_children())
 
         if ENABLE_STATUS_CHECK:
-            check_status(CURRENT_VERSION)
+            version, status, msg = check_status()
+            if version != CURRENT_VERSION:
+                tkm.showwarning("Warning",f'There is a new version, {version}. The current version is {CURRENT_VERSION}. Updating is recommened as it can cause you to get banned from DFM if you dont. Updates availbile from github.com/Jacrac04/DFM-Bot/releases')
+            if status == 'Normal':
+                if msg != 'None':
+                    tkm.showinfo("Information", msg)
+                # return False, (False, False)
+            elif status == 'Error':
+                if msg != 'None':
+                    tkm.showerror("Error", msg)
+                tkm.showerror("Unknown Error", "It is not recommended to use this until this is resolved.")
+            else:
+                tkm.showerror("Unknown Status", "It is not recommended to use this until this is resolved.")
 
         self.interface = Interface()
 
