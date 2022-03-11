b8ea86ef9930580b8d61765b50566ad7c26306c9
Jacrac04 <62761393+Jacrac04@users.noreply.github.com>
2022-03-11 20:11:42 +0000

    Change Version

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from src.generateTask import taskGenerator
 from ServerStatus.server_check import check_status
 
 
-CURRENT_VERSION = 'v4.1.2'
+CURRENT_VERSION = 'v5.0.0'
 ENABLE_STATUS_CHECK = True
 
 
